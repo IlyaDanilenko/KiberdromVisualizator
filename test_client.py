fee3bddc1f964a37329b3567633d0a01eda143af
Ilya Danilenko <ilya@eadsoft.com>
2022-04-20 17:32:27 +0300

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -1,5 +1,6 @@
 from socketio import Client
 import json
+from time import sleep
 
 objects = []
 objects.append({"id": 0, "type": "drone", "position": {"x": 0, "y": 1, "z": 4}, "yaw": 0})
@@ -10,7 +11,8 @@ sio = Client() # клиент SocketIO
 def response(data): # функция обработки ответа
     global objects
     if data["response"] == "ok": # если ответ получен, отправляем данные
-        objects[0]['position']['x'] += 0.02
+        sleep(0.01)
+        objects[0]['position']['x'] += 0.01
         sio.emit('position', json.dumps(objects))
 
 sio.connect('http://localhost:8080') # подключаемся 
